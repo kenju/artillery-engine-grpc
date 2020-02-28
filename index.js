97585589242e7699f40c9645ae0b67812031ebb6
kenju <ken901waga@gmail.com>
2020-02-28 23:20:38 +0900

    refactor: add #step and #compile function

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const A = require('async');
 const grpc = require('grpc')
 const protoLoader = require('@grpc/proto-loader')
+
+const { log: logger } = console
 
 function ArtilleryGRPCEngine(script, ee, helpers) {
   this.script = script
@@ -43,25 +46,56 @@ ArtilleryGRPCEngine.prototype.initGRPCClient = function initClient(config) {
 }
 
 ArtilleryGRPCEngine.prototype.createScenario = function createScenario(scenarioSpec, ee) {
-  const executeScenario = () => {
-    ee.emit('started')
+  const tasks = scenarioSpec.flow.map((ops) => this.step(ops, ee))
 
-    scenarioSpec.flow.forEach((flow) => {
-      Object.keys(flow).forEach((rpcName) => {
-        const args = flow[rpcName]
-        this.client[rpcName](args, (error, response) => {
-          if (error) {
-            ee.emit('error', error)
-          } else {
-            ee.emit('response', response)
-          }
-        })
+  return this.compile(tasks, scenarioSpec.flow, ee)
+}
+
+ArtilleryGRPCEngine.prototype.step = function step(ops, ee) {
+  if (ops.log) {
+    return (context, callback) => {
+      logger(this.helpers.template(ops.log, context))
+      return process.nextTick(() => { callback(null, context); });
+    };
+  }
+
+  // gRPC request
+  return gRPCRequest = (context, callback) => {
+    Object.keys(ops).map((rpcName) => {
+      const args = ops[rpcName]
+      this.client[rpcName](args, (error, response) => {
+        if (error) {
+          ee.emit('error', error)
+          return callback(err, context);
+        } else {
+          ee.emit('response', response)
+          return callback(null, context);
+        }
       })
     })
-
-    ee.emit('done')
   }
-  return executeScenario
+}
+
+ArtilleryGRPCEngine.prototype.compile = function compile(tasks, scenarioSpec, ee) {
+  return function scenario(initialContext, callback) {
+    const init = function init(next) {
+      ee.emit('started')
+      return next(null, initialContext)
+    }
+
+    const steps = [init].concat(tasks)
+
+    A.waterfall(
+      steps,
+      function done(err, context) {
+        if (err) {
+          debug(err)
+        }
+
+        return callback(err, context)
+      }
+    )
+  }
 }
 
 module.exports = ArtilleryGRPCEngine
